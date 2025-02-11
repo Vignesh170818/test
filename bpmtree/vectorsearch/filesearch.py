b2b5c9ce79c8168622ac122e22e01fc9979adec3
Vignesh170818 <v.c.mani@accenture.com>
2025-02-11 15:11:39 +0530

    modified filesearch.py file

--- bpmtree/vectorsearch/filesearch.py
+++ bpmtree/vectorsearch/filesearch.py
@@ -40,24 +40,24 @@ class PickleFileSearch:
         #     data = pickle.load(file)
         #     logger.info("PicklefileSize: %d bytes", os.path.getsize(self.indexfile_location))
 
-        file_path = self.indexfile_location
+        # file_path = self.indexfile_location
         # with open(file_path, "rb") as file:
         #     bcdf = pickle.load(file)
         #     Logger.info(bcdf)
-        with open(file_path, "rb") as f:
-            print(f.read(10))  # Read first 10 bytes to check format
+        # with open(file_path, "rb") as f:
+        #     print(f.read(10))  # Read first 10 bytes to check format
 
-        with open(file_path, "rb") as f:
-            data = pickle.load(f)
+        # with open(file_path, "rb") as f:
+        #     data = pickle.load(f)
  
-        if isinstance(data, dict):
-            df = pd.DataFrame(data["dataframe"])  # Extract DataFrame
-            logger.info(df.head())
+        # if isinstance(data, dict):
+        #     df = pd.DataFrame(data["dataframe"])  # Extract DataFrame
+        #     logger.info(df.head())
  
-            json_part = data.get("json_data", "{}")  # Extract JSON
-            logger.info(json.loads(json_part))
+            # json_part = data.get("json_data", "{}")  # Extract JSON
+            # logger.info(json.loads(json_part))
  
-        # bcdf = pd.read_pickle(self.indexfile_location)
+        bcdf = pd.read_pickle(self.indexfile_location)
          
         
         # with open(self.indexfile_location, "rb") as f:
